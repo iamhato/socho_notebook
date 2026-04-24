69c0c6b714668f804a010810148ac774ff078f65
hato <122075335+iamhato@users.noreply.github.com>
2026-04-24 15:19:01 +0900

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // あなたのリポジトリ名に合わせて設定
+  site: 'https://iamhato.github.io',
+  base: '/socho_notebook', // リポジトリ名が「my-band-site」なら '/my-band-site'
+});
